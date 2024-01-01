910dd87cc385bd16714d86145829da67cf5c1c6a
0xIrakli <butu.irakli@gmail.com>
2024-01-01 18:40:07 +0400

    Rewrite Header component (responsive)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,24 @@ import styles from './Header.module.css'
 import Cart from '../../assets/icon-cart.svg'
 import ProfilePlaceholder from '../../assets/user-placeholder.png'
 import { assetsBaseUrl } from '../../data'
+import { useState } from 'react'
+
+// yvela components qonda sxva margin zemot da qvemot amitom containershi chavsvi yvelaferi da ubralod padding davade containers
 
 function Header({ currentUser = null }) {
+	const [cartOpen, setCartOpen] = useState(false)
+
 	return (
-		<header className={styles.header}>
-			<a href="#">
-				<img src="/logo.svg" />
-			</a>
-			<div>
+		<div className={styles.container}>
+			<header className={styles.header}>
+				<div className={styles.hamburger}>
+					<span />
+					<span />
+					<span />
+				</div>
+				<a href="#">
+					<img src="/logo.svg" className={styles.logo} />
+				</a>
 				<nav className={styles.nav}>
 					<a href="#Collections">Collections</a>
 					<a href="#Men">Men</a>
@@ -17,21 +27,28 @@ function Header({ currentUser = null }) {
 					<a href="#About">About</a>
 					<a href="#Contact">Contact</a>
 				</nav>
-				<button className={styles.cart}>
-					<img src={Cart} />
-				</button>
-				{/* am shemtxvevashi profile button-ia da ara <a> radgan arvici ra xdeba dacherisas da button ufro martivia stylebistvis */}
-				<button className={styles.profile}>
-					<img
-						src={
-							currentUser
-								? assetsBaseUrl + currentUser.profileImage
-								: ProfilePlaceholder
-						}
-					/>
-				</button>
-			</div>
-		</header>
+				<div className={styles.flexEnd}>
+					<button className={styles.cart}>
+						<img src={Cart} />
+
+						{cartOpen && (
+							<div className={styles.dropDown}>
+								<span></span>
+							</div>
+						)}
+					</button>
+					<button className={styles.profile}>
+						<img
+							src={
+								currentUser
+									? assetsBaseUrl + currentUser.profileImage
+									: ProfilePlaceholder
+							}
+						/>
+					</button>
+				</div>
+			</header>
+		</div>
 	)
 }
 
